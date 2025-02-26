242ec9bb9adc11fea6060d94c094b041693b20ab
Mukesh Jaiswal <jais.mukesh@gmail.com>
2025-02-26 14:40:12 +0100

    Add static goal route page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Home Page</h1>
+
+<section>
+    <h2>Your Goals</h2>
+    <p>
+        List of goals cards goes here
+    </p>
+</section>
+
+<section>
+    <h2>Calendar</h2>
+    <p>
+        Calendar View goes here
+    </p>
+</section>
---
+++ src/routes/goal/+page.svelte
@@ -0,0 +1,10 @@
+<h1>Individual Goal Page</h1>
+
+<section>
+    Goals details of the card goes here
+</section>
+
+<section>
+    Calendar view goes here
+</section>
+
