2b9ba09c809edbf65c9aa89fba53742a505f0c42
Mukesh Jaiswal <jais.mukesh@gmail.com>
2025-03-04 18:18:48 +0100

    Create my own calendar component using shadcn component

---
+++ src/components/calendar/Calendar.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { getLocalTimeZone, today } from "@internationalized/date";
+    import { Calendar } from "$lib/components/ui/calendar";
+    
+    export let value = today(getLocalTimeZone());
+</script>
+
+<div class="w-full">
+    <div class="bg-white border rounded-lg shadow p-2">
+        <Calendar bind:value class="rounded-md" />
+    </div>
+</div>
